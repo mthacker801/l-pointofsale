a94abfff3dfba1c09fb1d7fcb9e3cf00dc3d69d1
Matthew <mtvanguard@gmail.com>
2018-09-19 18:19:39 -0600

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import {Sort} from '@angular/material';
+
+export interface Header {
+  description: string;
+  quantity: number;
+  unitPrice: number;
+  subtotal: number;
+  discount: number;
+  total: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +16,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pointOfSale';
+  // tslint:disable-next-line:quotemark
+  title = "Matt's Giftshop";
+
+  header: Header[] = [
+    {description: 'Guitar', quantity: 159, unitPrice: 6, subtotal: 24, discount: 4, total: 300},
+    {description: 'Gold Coin', quantity: 159, unitPrice: 6, subtotal: 24, discount: 4, total: 300},
+    {description: 'Boom Box', quantity: 159, unitPrice: 6, subtotal: 24, discount: 4, total: 300},
+    {description: 'Bracelet', quantity: 159, unitPrice: 6, subtotal: 24, discount: 4, total: 300},
+    {description: 'Laptop', quantity: 159, unitPrice: 6, subtotal: 24, discount: 4, total: 300},
+    {description: 'Sucker', quantity: 159, unitPrice: 6, subtotal: 24, discount: 4, total: 300},
+  ];
+
+  sortedData: Header[];
+
+  constructor() {
+    this.sortedData = this.header.slice();
+  }
+
+  sortData(sort: Sort) {
+    const data = this.header.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedData = data;
+      return;
+    }
+
+    this.sortedData = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'description': return compare(a.description, b.description, isAsc);
+        case 'quantity': return compare(a.quantity, b.quantity, isAsc);
+        case 'unitPrice': return compare(a.unitPrice, b.unitPrice, isAsc);
+        case 'subtotal': return compare(a.subtotal, b.subtotal, isAsc);
+        case 'discount': return compare(a.discount, b.discount, isAsc);
+        case 'total': return compare(a.total, b.total, isAsc);
+        default: return 0;
+      }
+    });
+  }
 }
+
+function compare(a, b, isAsc) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatSortModule } from '@angular/material/sort';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatGridListModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
